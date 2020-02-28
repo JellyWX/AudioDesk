e540f3c5335e53be8d0d6cfc4183c4dc6d7a57fc
jude-lafitteIII <judewrs@gmail.com>
2020-02-28 18:29:16 +0000

    changed button adding

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -19,11 +19,23 @@ void MainWindow::create_row_builder(const std::string& resource_path)
 
 void MainWindow::add_sound_button(const std::string& sound_name, const std::string& sound_path)
 {
-    Gtk::ButtonBox* row;
-    this->row_builder->get_widget("box", row);
+    auto row = new Gtk::ButtonBox(Gtk::ORIENTATION_HORIZONTAL);
+
+    auto label = new Gtk::Label(sound_name);
+    row->add(*label);
+
+    auto play = new Gtk::Button();
+    auto play_label = new Gtk::Label("Play Down Mic");
+    play->add(*play_label);
+    row->add(*play);
+
+    auto demo = new Gtk::Button();
+    auto demo_label = new Gtk::Label("Demo");
+    demo->add(*demo_label);
+    row->add(*demo);
 
     this->sound_box->add(*row);
-    row->show();
+    row->show_all();
 }
 
 void MainWindow::set_application(AudioDesk* app)
